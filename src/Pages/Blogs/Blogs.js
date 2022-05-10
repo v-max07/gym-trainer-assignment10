8ec415e0141725bf84181769453bc315bc3ac14a
v-max07 <vusandebnath09@gmail.com>
2022-05-11 03:22:48 +0600

    add my photo

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
             <div>
                 <h3>1. Difference between authorization and authentication?</h3>
                 <p>
-                    Simply put, authentication is the process of verifying who someone is, whereas authorization is the process of verifying what specific applications, files, and data a user has access to.In the digital world, authentication and authorization accomplish these same goals. Authentication is used to verify that users really are who they represent themselves to be. Once this has been confirmed, authorization is then used to grant the user permission to access different levels of information and perform specific functions, depending on the rules established for different types of users.
+                    Simply put, authentication is the process of verifying who someone is, whereas authorization is the process of verifying what specific applications, files, and data a user has access to.In the digital world, authentication and authorization accomplish these same goals. Authentication is used to verify that users really are who they represent themselves to be.
                 </p>
             </div>
             <div>
